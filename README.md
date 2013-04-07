ff40a98a1c78ac574429ca7403b3f954fb8d112e
David McInnes <david.mcinnes@uqconnect>
2013-04-07 22:26:37 +1000

    First commit, synced all the necessary files.
    Started on the home screen page, as well as creating some of the menus.

---
+++ README.md
@@ -0,0 +1,11 @@
+Interface Prototype
+========
+
+This is the early prototype for the UI elements.
+
+Colour Scheme and Text
+-----
+
+If you want to change the colour scheme, just go to res/values/colors.xml. The rest should be pretty self explanatory.
+
+If anyone wants to change any of the text (like the descriptions or something), use the res/values/strings.xml file. Again, rest should be pretty self explanatory.
---
+++ src/com/example/wifimanagerprototype/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.wifimanagerprototype;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+       /* R.id.home_map_container.setOnTouchListener(new OnTouchListener() {
+        	@Override
+            public boolean onTouch(View v, MotionEvent event) {
+            	// TODO Auto-generated method stub
+            	return false;
+            }
+       });*/
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+            	startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            case R.id.action_home:
+            	startActivity(new Intent(this, MainActivity.class));
+            	return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    
+    public void mapClickEvent(View view) {
+    	startActivity(new Intent(this, MapActivity.class));
+    }
+    
+    public void compassClickEvent(View view) {
+    	startActivity(new Intent(this, CompassActivity.class));
+    }
+    
+    public void cameraClickEvent(View view) {
+    	startActivity(new Intent(this, CameraActivity.class));
+    }
+}
