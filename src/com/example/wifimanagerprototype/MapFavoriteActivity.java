166a6cc46d6d510cb53e53d25168e7904fef31e3
David McInnes <david.mcinnes@uqconnect>
2013-04-08 12:19:47 +1000

    Added some new screens for the Maps.

---
+++ src/com/example/wifimanagerprototype/MapFavoriteActivity.java
@@ -0,0 +1,91 @@
+package com.example.wifimanagerprototype;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MapFavoriteActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map_favorite);
+		
+		ListView listview = (ListView) findViewById(R.id.list_favorites);
+		final ArrayList<String> list = new ArrayList<String>();
+		list.add("Eduroam");
+		list.add("WiFi");
+		
+		listview.setAdapter(new StableArrayAdapter(this, android.R.layout.simple_list_item_1, list));
+		
+		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				final String item = (String) parent.getItemAtPosition(position);
+			}
+		});
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        	case R.id.action_az:
+        		startActivity(new Intent(this, MapAZActivity.class));
+                return true;
+        	case R.id.action_favorite:
+        		startActivity(new Intent(this, MapFavoriteActivity.class));
+                return true;
+        	case R.id.action_relocate:
+        		startActivity(new Intent(this, MapRelocateActivity.class));
+                return true;
+            case R.id.action_settings:
+            	startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+	
+	/** Code from http://www.vogella.com/articles/AndroidListView/article.html **/
+	private class StableArrayAdapter extends ArrayAdapter<String> {
+	    HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+	    public StableArrayAdapter(Context context, int textViewResourceId,
+	        List<String> objects) {
+	      super(context, textViewResourceId, objects);
+	      for (int i = 0; i < objects.size(); ++i) {
+	        mIdMap.put(objects.get(i), i);
+	      }
+	    }
+
+	    @Override
+	    public long getItemId(int position) {
+	      String item = getItem(position);
+	      return mIdMap.get(item);
+	    }
+
+	    @Override
+	    public boolean hasStableIds() {
+	      return true;
+	    }
+	}
+
+}
--- src/com/example/wifimanagerprototype/MapRelocateActivity.java
+++ src/com/example/wifimanagerprototype/MapRelocateActivity.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MapActivity extends Activity {
+public class MapRelocateActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_map);
+		setContentView(R.layout.activity_map_relocate);
 	}
 
 	@Override
@@ -21,7 +21,7 @@ public class MapActivity extends Activity {
 		return true;
 	}
 	
-    @Override
+	@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
         	case R.id.action_az:
