ccc7b8eca53b1b0286e318af815048c19b62faf5
Steven Sheriff <sjs250@gmail>
2013-05-05 15:58:20 +1000

    modified point class to provide for bad values

--- src/com/example/wifimanagerprototype/Point.java
+++ src/com/example/wifimanagerprototype/Point.java
@@ -5,8 +5,10 @@ public class Point {
     public double latitude = 0f;
     public String description;
     public float x, y = 0;
-
+    
     public Point(double lat, double lon, String desc) {
+    	if (lat > 90 || lat < -90 || lon > 180 || lon < -180)
+    		return;
         this.latitude = lat;
         this.longitude = lon;
         this.description = desc;
