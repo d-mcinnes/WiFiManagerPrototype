5da5ed31022f1c704f7f239c028d074855882e10
David McInnes <david.mcinnes@uqconnect>
2013-05-16 10:08:17 +1000

    Added info Button
    - Added Info Button to the Camera Heatmap Page.

--- src/com/example/wifimanagerprototype/MapGreenActivity.java
+++ src/com/example/wifimanagerprototype/MapGreenActivity.java
@@ -1,8 +1,12 @@
 package com.example.wifimanagerprototype;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
 import android.view.Menu;
 import android.view.View;
@@ -10,11 +14,14 @@ import android.widget.CheckBox;
 import android.widget.ImageView;
 
 public class MapGreenActivity extends Activity {
+	private Activity activity;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map_green);
+		
+		this.activity = this;
 	}
 
 	@Override
@@ -25,20 +32,40 @@ public class MapGreenActivity extends Activity {
 	}
 	
 	public void onHeatmapCheck(View view) {
-		//R.id.checkbox_compass.setChecked(false);
 		CheckBox checkbox = (CheckBox) findViewById(R.id.checkbox_heatmap);
 		ImageView imagePrototype = (ImageView) findViewById(R.id.prototype_image);
 		if(checkbox.isChecked()) {
-			System.out.println("Checked");
 			Drawable d = getResources().getDrawable(R.drawable.prototype_map_display);
 			imagePrototype.setImageDrawable(d);
 		} else {
-			System.out.println("Not Checked");
 			Drawable d = getResources().getDrawable(R.drawable.prototype_map_display_dots);
 			imagePrototype.setImageDrawable(d);
 		}
-		//checkbox.setChecked(false);
-		//startActivity(new Intent(this, CompassTestActivity.class));
 	}
+	
+	public Activity mapGetActivity() {
+		return this.activity;
+	}
+	
+	public void loadInfo(View view) {
+    	DialogFragment newFragment = new InfoPaneFragment();
+        newFragment.show(getFragmentManager(), "missiles");
+    }
+    
+	@SuppressLint("ValidFragment")
+	private class InfoPaneFragment extends DialogFragment {
+    	@Override
+    	public Dialog onCreateDialog(Bundle savedInstanceState) {
+    		AlertDialog.Builder builder = new AlertDialog.Builder(mapGetActivity());
+    		builder.setMessage(getString(R.string.info_map))
+    			   .setTitle("Info");
+    		builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+    	           public void onClick(DialogInterface dialog, int id) {
+    	               // User clicked OK button
+    	           }
+    	       });
+			return builder.create();
+    	}
+    }
 
 }
